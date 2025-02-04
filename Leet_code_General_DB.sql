9db86010a9ff9d3792a7dcc71be9da7d19d5d020
Ann Nyamu <anyamu@hq.ekenya.co.ke>
2025-02-04 19:12:20 +0300

    Use of Enum

--- Leet_code_General_DB.sql
+++ Leet_code_General_DB.sql
@@ -105,11 +105,43 @@ INSERT INTO EmployeeUNI(id, unique_id)
  (12, 1, 910),
  (13, 2, 970);
  
- CREATE TABLE Weather(
-  transaction_id INT,
- visit_id INT,
- amount INT
+ CREATE TABLE Weathers(
+  id INT,
+ recordDate DATE,
+ temperature INT
  );
  
+ INSERT INTO Weathers(id, recordDate, temperature)
+ VALUES
+ (1, "2015-01-01", 10),
+ (2, "2015-01-02", 25),
+ (3, "2015-01-03", 20),
+ (4, "2015-01-04", 30),
+ (5, "2015-01-06", 35),
+ (6, "2015-01-04", 40);
+ 
+ CREATE TABLE Activities(
+ machine_id INT, 
+ process_id INT,
+ activity_type ENUM('Start','End') NOT NULL,
+ timestamp FLOAT 
+ );
+ 
+ ALTER TABLE Activities MODIFY activity_type ENUM('start', 'end');
+ 
+ INSERT INTO Activity(machine_id, process_id, activity_type, timestamp)
+ VALUES
+ (0, 0, 'start', 0.712),
+ (0, 0, 'end', 1.520),
+ (0, 1, 'start', 3.140),
+ (0, 1, 'end', 4.120),
+ (1, 0, 'start', 0.550),
+ (1, 0, 'end', 1.550),
+ (1, 1, 'start', 0.430),
+ (1, 1, 'end', 1.420),
+ (2, 0, 'start', 4.100),
+ (2, 0, 'end', 4.512),
+ (2, 1, 'start', 2.500),
+ (2, 1, 'end', 5.000); 
  
  SHOW TABLES;
